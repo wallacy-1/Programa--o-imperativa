900bc89fafd2857e57fe5977bee99ba7ca4bfe46
wallacysantos <wwallacy@live.com>
2021-10-25 21:11:57 -0300

    aula 4 dia 25/10/2021

---
+++ aula4-DIA-25-10-2021/ex2.js
@@ -0,0 +1,11 @@
+// coloque comentarios    -- exercicio guiado com a prof
+let nome = "Danilo";
+let idade = 25;
+let usuarioAtivo = true;
+
+
+
+
+console.log(nome);
+console.log(idade);
+console.log(usuarioAtivo);
---
+++ aula4-DIA-25-10-2021/ex3.js
@@ -0,0 +1,13 @@
+let nome = "josé";
+let idade = 27;
+let peso = 83.5;
+let altura = 1.70;
+let plano = true;
+
+altura = altura * altura;
+peso = peso * 10;
+
+let imc = peso / altura^2;
+
+
+console.log(nome, " tem ", idade, " anos e seu indice de massa corporal e de ",imc);
